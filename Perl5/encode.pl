f3aa87be3d4c637124e861ac1d5954b612e9e74a
Seaoak <seaoak2003@gmail.com>
2019-04-28 11:56:11 +0900

    Refactoring

--- Perl5/encode.pl
+++ Perl5/encode.pl
@@ -1,24 +1,29 @@
 {
     use Encode qw();
 
+    my $make_helper = sub {
+        die unless $#_ == 1;
+        die if grep {not $_} @_;
+        my ($func, $encoding) = @_;
+        die unless 1 == grep {$_ eq $encoding} ('UTF-8', 'shiftjis', 'cp932', 'euc-jp', 'iso-2022-jp', 'iso-2022-jp-1');
+        my $helper = sub {
+            die unless @_;
+            die if grep {not defined $_} @_;
+            die if grep {ref $_} @_;
+            my @buf = map {'' . $_} @_; # shallow copy
+            my @result = map {$func->($encoding, $_, Encode::FB_CROAK)} @buf;
+            die if grep {not defined $_} @result;
+            return wantarray ? @result : $result[0];
+        };
+        return $helper;
+    };
+
     sub encode_to_shiftjis {
-        die unless @_;
-        die if grep {not defined $_} @_;
-        die if grep {ref $_} @_;
-        my @buf = map {'' . $_} @_; # shallow copy
-        my @result = map {Encode::encode('shiftjis', $_, Encode::FB_CROAK)} @buf;
-        die if grep {not defined $_} @result;
-        return wantarray ? @result : $result[0];
+        return $make_helper->(\&Encode::encode, 'shiftjis')->(@_);
     }
 
     sub decode_from_shiftjis {
-        die unless @_;
-        die if grep {not defined $_} @_;
-        die if grep {ref $_} @_;
-        my @buf = map {'' . $_} @_; # shallow copy
-        my @result = map {Encode::decode('shiftjis', $_, Encode::FB_CROAK)} @buf;
-        die if grep {not defined $_} @result;
-        return wantarray ? @result : $result[0];
+        return $make_helper->(\&Encode::decode, 'shiftjis')->(@_);
     }
 }
 
